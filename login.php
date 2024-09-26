f227b48465e97c6a65ab0e49ee012257239ea753
Bhanu Teja <ybhanuteja555@gmail.com>
2024-09-26 23:23:48 +0530

    login page is completed

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Main</title>
+</head>
+<body>
+	<?php
+
+	if ($_SERVER['REQUEST_METHOD']=='POST') {
+		// code...
+		if ($_POST['action']=='login') {
+			// code...
+			$conn = new mysqli("localhost","root","","nani_bank_website");
+
+			if($conn->connect_error)
+			{
+				die("db connection is failed".$conn->connect_error);
+			}
+
+			$user_name = $_POST['user_name'];
+			$password = $_POST['password'];
+
+			$query = "select * from bank_users where user_name=? and password=?";
+			$stmt = $conn->prepare($query);
+			$stmt->bind_param("ss",$user_name,$password);
+			
+			$result = $stmt->get_result();
+
+			if($result->num_rows > 0)
+			{
+				header(main_page.html);
+			}
+			else
+			{
+				echo "The user_name or password is incorrect";
+			}
+ 
+		}
+		elseif ($_POST['action']=='Create_Account') {
+			// code...
+			header(location:create_account.html);
+			exit();
+		}
+	}
+	?>
+
+</body>
+</html>
